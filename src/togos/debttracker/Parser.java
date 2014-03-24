a33463c5e2080890b29879ca92ecd53d1f4bb270
TOGoS <togos00@gmail.com>
2014-03-23 22:08:59 -0500

    More wrx on the parser.

--- src/togos/debttracker/Parser.java
+++ src/togos/debttracker/Parser.java
@@ -5,31 +5,117 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Parser
 {
-	class Entity {
+	public static class Entity {
+		public final String id;
 		public String name = "unnamed entity";
 		public BigDecimal balance = BigDecimal.valueOf(0, 2);
+		
+		public Entity( String id ) {
+			this.id = id;
+		}
 		
 		public void addBalance( BigDecimal delta ) {
 			balance = balance.add(delta);
 		}
 	}
 	
-	Map<String,Entity> entities = new LinkedHashMap<String,Entity>(); 
+	public Map<String,Entity> entities = new LinkedHashMap<String,Entity>(); 
 	
 	// TODO: Eventually put a dece tokenizer up in here
-	static final Pattern ENTITY_PATTERN = Pattern.compile("entity \\s+ ([^\\s+]) \\s+ : \\s+ name \\s* @ \\s* \"([^\"]*)\"", Pattern.COMMENTS);
+	static final Pattern ENTITY_PATTERN = Pattern.compile(
+		"entity \\s+ ([^\\s]+) " +
+		"(?: \\s+ : \\s+ name \\s* @ \\s* \"([^\"]*)\" )?",
+		Pattern.COMMENTS);
+	static final Pattern ALIAS_PATTERN = Pattern.compile("alias \\s+ ([^\\s]+) \\s+ = \\s+ ([^\\s]+)", Pattern.COMMENTS);
+	static final Pattern TRANSACTION_PATTERN = Pattern.compile(
+		"(\\d\\d\\d\\d-\\d\\d-\\d\\d)" +
+		"\\s+ : \\s+ (\\d+) " +
+		"\\s+ @ \\s+ ((?!->).+?) " +
+		"\\s+ -> \\s+ ([^;]+?) " +
+		"(?: \\s+ ; (.*))?",
+		Pattern.COMMENTS);
+	
+	protected static Entity getEntity( Map<String,Entity> aliases, String name ) {
+		Entity e = aliases.get(name);
+		if( e == null ) throw new RuntimeException("Reference to undefined entity '"+name+"'");
+		return e;
+	}
+	
+	protected static Entity[] getEntities( Map<String,Entity> aliases, String list ) {
+		String[] names = list.split(",");
+		Entity[] entities = new Entity[names.length];
+		for( int i=0; i<names.length; ++i ) {
+			entities[i] = getEntity(aliases, names[i].trim());
+		}
+		return entities;
+	}
+	
+	protected static BigDecimal[] divide( BigDecimal amt, int parts ) {
+		BigDecimal d = amt.divide(new BigDecimal(parts), RoundingMode.HALF_EVEN);
+		BigDecimal remainder = amt.subtract(d.multiply(new BigDecimal(parts)));
+		// For now just lump all remainder on one dude
+		// would be better to split remainder as even as possible
+		BigDecimal[] dex = new BigDecimal[parts];
+		dex[0] = d.add(remainder);
+		for( int i=1; i<parts; ++i ) {
+			dex[i] = d;
+		}
+		return dex;
+	}
 	
 	public void readFile( BufferedReader br ) throws IOException {
-		// TODO
+		Map<String, Entity> aliases = new HashMap<String, Entity>(entities);
+		String line;
+		Matcher m;
+		while( (line = br.readLine()) != null ) {
+			line = line.trim();
+			if( "".equals(line) || line.startsWith("#") ) continue;
+			
+			if( (m = ENTITY_PATTERN.matcher(line)).matches() ) {
+				String entityId = m.group(1);
+				Entity e = new Entity(entityId);
+				e.name = m.group(2) == null ? entityId : m.group(2);
+				entities.put(entityId, e);
+				aliases.put(entityId, e);
+			} else if( (m = ALIAS_PATTERN.matcher(line)).matches() ) {
+				String newId = m.group(1);
+				String oldId = m.group(2);
+				Entity e = getEntity(aliases, oldId);
+				aliases.put(newId, e);
+			} else if( (m = TRANSACTION_PATTERN.matcher(line)).matches() ) {
+				//String dateStr = m.group(1);
+				String amtStr = m.group(2);
+				String payerStr = m.group(3);
+				String receiverStr = m.group(4);
+				//String comment = m.group(5);
+				Entity[] payers = getEntities(aliases, payerStr);
+				Entity[] recipients = getEntities(aliases, receiverStr);
+				BigDecimal amount = new BigDecimal(amtStr);
+				BigDecimal[] paidAmounts = divide(amount, payers.length);
+				for( int i=0; i<payers.length; ++i ) {
+					payers[i].addBalance(paidAmounts[i]);
+				}
+				BigDecimal[] receivedAmounts = divide(amount, recipients.length);
+				for( int i=0; i<recipients.length; ++i ) {
+					recipients[i].addBalance(receivedAmounts[i].negate());
+				}
+			} else {
+				throw new RuntimeException("Unrecognized line: " + line);
+			}
+		}
 	}
 	
 	public void readFile( InputStream is ) throws IOException {
@@ -46,6 +132,14 @@ public class Parser
 			} finally {
 				fr.close();
 			}
+		}
+	}
+	
+	public void readString( String text ) {
+		try {
+			readFile(new BufferedReader(new StringReader(text)));
+		} catch( IOException e ) {
+			throw new RuntimeException(e);
 		}
 	}
 	
---
+++ test/togos/debttracker/ParserTest.java
@@ -0,0 +1,48 @@
+package togos.debttracker;
+
+import java.math.BigDecimal;
+
+import junit.framework.TestCase;
+import togos.debttracker.Parser.Entity;
+
+public class ParserTest extends TestCase
+{
+	protected static final String TED_EID = "ccd4d1b4-1a3d-460c-902f-681dcc0194fd";
+	protected static final String BOB_EID = "1be5ab07-7829-4643-899b-bf5e4bdb12b8";
+	
+	protected static final String introText =
+		"# A comment line!\n" +
+		"entity "+TED_EID+" : name @ \"Ted\"\n" +
+		"entity "+BOB_EID+" : name @ \"Bob Q\"\n" +
+		"\n" + // A blank line!
+		"alias Ted = "+TED_EID+"\n" +
+		"alias BobQ = "+BOB_EID+"\n" +
+		"\n";
+	
+	protected static final String transactionText =
+		introText +
+		"2014-01-03 : 600 @ BobQ -> BobQ, Ted ; rent\n";
+	
+	public void testParseIntro() {
+		Parser p = new Parser();
+		p.readString(introText);
+		assertEquals(2, p.entities.size());
+		{
+			Entity ted = p.entities.get("ccd4d1b4-1a3d-460c-902f-681dcc0194fd");
+			assertEquals("ccd4d1b4-1a3d-460c-902f-681dcc0194fd", ted.id);
+			assertEquals("Ted", ted.name);
+		}
+		{
+			Entity bob = p.entities.get("1be5ab07-7829-4643-899b-bf5e4bdb12b8");
+			assertEquals("1be5ab07-7829-4643-899b-bf5e4bdb12b8", bob.id);
+			assertEquals("Bob Q", bob.name);
+		}
+	}
+	
+	public void testTransactions() {
+		Parser p = new Parser();
+		p.readString(transactionText);
+		assertEquals(new BigDecimal("-300.00"), p.entities.get(TED_EID).balance);
+		assertEquals(new BigDecimal("+300.00"), p.entities.get(BOB_EID).balance);
+	}
+}
